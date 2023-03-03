90ed356606073c032342f5e9de13e9a2867d9712
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-03 07:50:39 +0500

    x15

--- logic15.py
+++ logic15.py
@@ -6,4 +6,6 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    return a==int(a) and abs(a)>=100 and abs(a)<=999 and (abs(a)//100 + abs(a)//10%10 + abs(a)%10)%2 ==1
+
+print(main(997))
