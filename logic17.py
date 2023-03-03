323cb22955358c4831d5e81ca5015b684422450c
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-03 09:13:56 +0500

    x17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,7 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    return a==int(a) and (abs(a)//10000>abs(a)//1000%10>abs(a)//100%10>abs(a)//10%10>abs(a)%10 or \
+                          abs(a)//10000<abs(a)//1000%10<abs(a)//100%10<abs(a)//10%10<abs(a)%10)
+
+print(main(54321))
