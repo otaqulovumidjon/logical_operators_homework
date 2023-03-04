09236210abb31fdeed6161d86450d93dc2902dc4
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-04 11:42:21 +0500

    fix 19

--- logic19.py
+++ logic19.py
@@ -3,11 +3,15 @@ def main(x):
     Given a two digit integer x, return true if x is palindrome integer.
     An integer is a palindrome, when it reads the same backward as forward.
 
+    Ikki xonali butun x son berilgan bo'lsa, agar x palindromli butun son bo'lsa, true qiymatini qaytaring.
+    Butun son palindrom bo'lib, u oldinga qarab o'qisa.
+
     Args:
         x(int): parameter x
     Returns:
         bool: answer
     """
-    return x == int(x) and x>=10 and x<=99 and x == x//10 + (x%10)*10
-    
-print(main(33))
+    n1 = x%10
+    n2 = x//10
+    n = n1*10 + n2
+    return x == n
