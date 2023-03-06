bb096674f1aeb5a013f4dd4664ec255a7bddfad9
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-06 08:11:16 +0500

    x20

--- logic20.py
+++ logic20.py
@@ -2,12 +2,16 @@ def main(n):
     """
     A number consisting of one and zero is given (the number starts at once). 
     If the number of ones is greater than zero, true, otherwise False is returned.
+
+    Bir va noldan iborat raqam berilgan (raqam birdan boshlanadi).
+    Agar birliklar soni noldan katta bo'lsa, rost, aks holda False qaytariladi.
+    
     Args:
         n(int): parameter n
     Returns:
         bool: answer
     """
 
-    return n==int(n) and n>=10000 and n<=99999 and n//10000 + n//1000%10 + n//100%10 + n//10%10 + n%10 > 2
+    return n%10 > 0
 
-print(main(10011))
+print(main(10001))
