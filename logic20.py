b176ae53f8648ba04912f1297af3f58ce8831722
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-06 09:34:52 +0500

    x20

--- logic20.py
+++ logic20.py
@@ -11,7 +11,26 @@ def main(n):
     Returns:
         bool: answer
     """
+    x1=n//10000
+    x2=n//1000%10
+    x3=n//100%10
+    x4=n//10%10
+    x5=n%10
 
-    return n%10 > 0
+    a = 0
+    a = a+(x1%2==1)
+    a = a+(x2%2==1)
+    a = a+(x3%2==1)
+    a = a+(x4%2==1)
+    a = a+(x5%2==1)
+
+    b = 0
+    b = b+(x1%2==0)
+    b = b+(x2%2==0)
+    b = b+(x3%2==0)
+    b = b+(x4%2==0)
+    b = b+(x5%2==0)
+    
+    return a > b
 
 print(main(10011))
